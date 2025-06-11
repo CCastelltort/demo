5dfc0a2a556b638e1c3b2fb2b357c94588168591
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-06-11 12:43:31 +0200

    added preserve scroll attribute

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,9 +1,23 @@
 <template>
     <h1>Users</h1>
 
+    <div style="margin-top: 800px;">
+        <p>The current time is {{ time }}.</p>
+    </div>
+
+
+    <Link href="/users" class="text-blue-500" preserve-scroll>
+        Refresh
+    </Link>
+
 </template>
 
 <script setup>
+import { Link } from '@inertiajs/vue3'
+
+defineProps({
+    time: String
+});
 
 </script>
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import { createApp, h } from 'vue'
+import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/vue3'
 
 createInertiaApp({
