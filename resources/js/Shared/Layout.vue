ca7eaba8b7b9193e40f2ea1f723f4d90ece08711
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-06-11 13:27:45 +0200

    added shared data to layout

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -2,7 +2,14 @@
     <div>
         <section class="p-6 bg-gray-200">
             <header class="flex justify-between">
-                <h1 class="font-bold text-lg">My App</h1>
+                <div class="flex items-center">
+                    <h1 class="font-bold text-lg">My App</h1>
+                    
+                    <p class="text-sm ml-4">
+                        Welcome Back, {{ user.username }}!
+                    </p>
+                </div>
+                
                 <Nav></Nav>
             </header>
         </section>
@@ -18,7 +25,10 @@
 
 <script setup>
 import Nav from "./Nav.vue";
+import { usePage } from '@inertiajs/vue3'
 
+const { props } = usePage()
+const user = props.auth.user
 
 </script>
 
